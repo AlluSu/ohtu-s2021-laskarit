8ec2bd64646c0e5b1eff901833ffeb33de77335d
Aleksi Suuronen <aleksi.suuronen@outlook.com>
2021-11-09 08:51:14 +0200

    Viikko2 tehtävä 2

---
+++ viikko2/project-reader/src/project_reader.py
@@ -0,0 +1,15 @@
+from urllib import request
+from project import Project
+
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        import toml
+        # tiedoston merkkijonomuotoinen sisältö
+        content = request.urlopen(self._url).read().decode("utf-8")
+        new_content = toml.loads(content)
+        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
+        return Project(new_content["tool"]["poetry"]["name"], new_content["tool"]["poetry"]["description"], new_content["tool"]["poetry"]["dependencies"], new_content["tool"]["poetry"]["dev-dependencies"])
