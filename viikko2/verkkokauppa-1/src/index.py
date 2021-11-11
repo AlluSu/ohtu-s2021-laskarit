7b760e07603517a6c81f31e93a3d54937d5140ea
Aleksi Suuronen <aleksi.suuronen@outlook.com>
2021-11-11 15:19:02 +0200

    Lisätty tehtävä 12

--- viikko2/verkkokauppa-1/src/index.py
+++ viikko2/verkkokauppa-1/src/index.py
@@ -1,15 +1,11 @@
 from kauppa import Kauppa
-from kirjanpito import Kirjanpito
+from kirjanpito import kirjanpito as default_kirjanpito
 from varasto import Varasto
 from pankki import Pankki
 from viitegeneraattori import Viitegeneraattori
 
 
 def main():
-    viitegeneraattori = Viitegeneraattori()
-    kirjanpito = Kirjanpito()
-    varasto = Varasto(kirjanpito)
-    pankki = Pankki(kirjanpito)
     #kauppa = Kauppa(varasto, pankki, viitegeneraattori)
     kauppa = Kauppa()
 
@@ -30,7 +26,7 @@ def main():
     kauppa.tilimaksu("Arto Vihavainen", "3425-1652")
 
     # kirjanpito
-    for tapahtuma in kirjanpito.tapahtumat:
+    for tapahtuma in default_kirjanpito.tapahtumat:
         print(tapahtuma)
 
 
