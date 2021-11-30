3396f2607ad91eb64c0ed559762abc5e47801e61
Aleksi Suuronen <aleksi.suuronen@outlook.com>
2021-11-30 13:44:32 +0200

    Vk5 t3, hieman refaktoroitu

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,33 +1,41 @@
 class TennisGame:
     MAX_SCORE = 4
+    POINTS_IN_BEGINNING = 0
     
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.m_score1 = self.POINTS_IN_BEGINNING
+        self.m_score2 = self.POINTS_IN_BEGINNING
 
     def won_point(self, player_name):
         if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
+            self.m_score1 += 1
         else:
-            self.m_score2 = self.m_score2 + 1
+            self.m_score2 += 1
+
+    def is_tie(self, player1_score, player2_score):
+        return player1_score == player2_score
+
+    def get_tie_results(self, result):
+        result_dict = {
+            0:"Love-All",
+            1:"Fifteen-All",
+            2:"Thirty-All",
+            3:"Forty-All",
+            4:"Deuce"
+        }
+        if result > 3 or result < 0:
+            return result_dict[4]
+        return result_dict[result]
+
 
     def get_score(self):
         score = ""
         temp_score = 0
 
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            elif self.m_score1 == 3:
-                score = "Forty-All"
-            else:
-                score = "Deuce"
+        if self.is_tie(self.m_score1, self.m_score2):
+            score = self.get_tie_results(self.m_score1)
         elif self.m_score1 >= self.MAX_SCORE or self.m_score2 >= self.MAX_SCORE:
             minus_result = self.m_score1 - self. m_score2
 
