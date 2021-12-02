5b4f379a660661f05075c4df23aa81ad1643e4e0
Aleksi Suuronen <aleksi.suuronen@outlook.com>
2021-12-02 09:19:01 +0200

    vk t4 aihio

--- viikko5/laskin/src/kayttoliittyma.py
+++ viikko5/laskin/src/kayttoliittyma.py
@@ -13,30 +13,59 @@ class Summa:
         self._sovelluslogiikka = sovelluslogiikka
         self._io = io
 
+    #TODO
     def suorita(self):
-        # jotain
+        pass
 
+    #TODO tehtävä 5
+    def kumoa(self):
+        pass
+
+class Erotus:
+    def __init__(self, sovelluslogiikka, io):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._io = io
+
+    #TODO
+    def suorita(self):
+        pass
+
+    #TODO Tehtävä 5
+    def kumoa(self):
+        pass
+
+class Nollaus:
+    def __init__(self, sovelluslogiikka, io):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._io = io
+
+    def suorita(self):
+        pass
+
+    #TODO tehtävä 5
+    def kumoa(self):
+        pass
 
 
 class Kayttoliittyma:
-    def __init__(self, sovellus, root):
-        self._sovellus = sovellus
+    def __init__(self, sovelluslogiikka, root):
+        self._sovelluslogiikka = sovelluslogiikka
         self._root = root
-
+        
         self._komennot = {
             Komento.SUMMA: Summa(sovelluslogiikka, self._lue_syote),
             Komento.EROTUS: Erotus(sovelluslogiikka, self._lue_syote),
             Komento.NOLLAUS: Nollaus(sovelluslogiikka, self._lue_syote),
-            Komento.KUMOA: Kumoa(sovelluslogiikka, self._lue_syote)
+            #Komento.KUMOA: Kumoa(sovelluslogiikka, self._lue_syote)
         }
-
+        
+    # ...
     # JOTAIN?
-    def _lue_syote(self):
-        return self._syote_kentta.get()
+    # ...
 
     def kaynnista(self):
         self._tulos_var = StringVar()
-        self._tulos_var.set(self._sovellus.tulos)
+        self._tulos_var.set(self._sovelluslogiikka.tulos)
         self._syote_kentta = ttk.Entry(master=self._root)
 
         tulos_teksti = ttk.Label(textvariable=self._tulos_var)
@@ -74,6 +103,9 @@ class Kayttoliittyma:
         self._nollaus_painike.grid(row=2, column=2)
         self._kumoa_painike.grid(row=2, column=3)
 
+    def _lue_syote(self):
+        return self._syote_kentta.get()
+
     def _suorita_komento(self, komento):
         komento_olio = self._komennot[komento]
         komento_olio.suorita()
@@ -98,10 +130,10 @@ class Kayttoliittyma:
         
         self._kumoa_painike["state"] = constants.NORMAL
 
-        if self._sovellus.tulos == 0:
+        if self._sovelluslogiikka.tulos == 0:
             self._nollaus_painike["state"] = constants.DISABLED
         else:
             self._nollaus_painike["state"] = constants.NORMAL
 
         self._syote_kentta.delete(0, constants.END)
-        self._tulos_var.set(self._sovellus.tulos)
+        self._tulos_var.set(self._sovelluslogiikka.tulos)
